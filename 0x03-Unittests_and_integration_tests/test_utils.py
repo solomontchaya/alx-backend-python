fa2bdf58baa94bac444b22bfbbcb2439985adf25
Solomon Tchaya <tchayasolomon2000@gmail.com>
2025-09-15 22:14:12 +0200

    Week 4

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -80,3 +80,6 @@ class TestMemoize(unittest.TestCase):
             self.assertEqual(result1, 42)
             self.assertEqual(result2, 42)
             mock_method.assert_called_once()
+
+if __name__ == '__main__':
+    unittest.main()
