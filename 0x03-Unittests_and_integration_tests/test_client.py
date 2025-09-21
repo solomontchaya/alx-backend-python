7b55baed70d494e8dc1d7e8fa9d722275dc42698
Happiness Obochi <obochihappiness180@gmail.com>
2025-09-22 00:16:24 +0100

    client testing

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,12 +3,26 @@
 """
 import unittest
 from unittest.mock import patch
+from parameterized import parameterized
 from parameterized import parameterized_class
 
 from client import GithubOrgClient
 from fixtures import org_payload, repos_payload, expected_repos, apache2_repos
 
 
+class TestGithubOrgClient(unittest.TestCase):
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch("client.get_json")
+    def test_org(self, org_name, mock_get_json):
+        mock_get_json.return_value = {"org": org_name}
+        client = GithubOrgClient(org_name)
+        self.assertEqual(client.org, {"org": org_name})
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}"
+        )
 @parameterized_class([
     {
         "org_payload": org_payload,
