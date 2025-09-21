56200cbb35cd1c07346598b2c1fef4e6a2220990
Solomon Tchaya <tchayasolomon2000@gmail.com>
2025-09-21 11:58:09 +0200

    Week 4

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,7 +4,7 @@ Unit tests for client module
 """
 import unittest
 from unittest.mock import (
-    patch, Mock, PropertyMock
+    patch, Mock, PropertyMock, call
 )
 from parameterized import parameterized, parameterized_class
 from client import GithubOrgClient
@@ -103,7 +103,6 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         """Set up class method to mock requests.get"""
         cls.get_patcher = patch('client.requests.get')
         cls.mock_get = cls.get_patcher.start()
-        cls.addClassCleanup(cls.get_patcher.stop)
 
         def side_effect(url):
             mock_response = Mock()
@@ -117,6 +116,11 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
 
         cls.mock_get.side_effect = side_effect
 
+    @classmethod
+    def tearDownClass(cls):
+        """Tear down class method to stop the patcher"""
+        cls.get_patcher.stop()
+
     def test_public_repos_integration(self):
         """Integration test for public_repos method"""
         client = GithubOrgClient("google")
@@ -129,7 +133,7 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
             "https://api.github.com/orgs/google",
             "https://api.github.com/orgs/google/repos",
         ]
-        actual_urls = [call_args[0][0] for call_args in self.mock_get.call_args_list]
+        actual_urls = [args[0] for args, _ in self.mock_get.call_args_list]
 
         for expected_url in expected_urls:
             self.assertIn(expected_url, actual_urls)
@@ -146,7 +150,7 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
             "https://api.github.com/orgs/google",
             "https://api.github.com/orgs/google/repos",
         ]
-        actual_urls = [call_args[0][0] for call_args in self.mock_get.call_args_list]
+        actual_urls = [args[0] for args, _ in self.mock_get.call_args_list]
 
         for expected_url in expected_urls:
             self.assertIn(expected_url, actual_urls)
