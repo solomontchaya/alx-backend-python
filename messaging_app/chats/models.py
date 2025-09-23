9bb38c39785902e2400afc97883e596014dff04c
Happiness Obochi <obochihappiness180@gmail.com>
2025-09-23 12:02:02 +0100

    migration settled

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -3,10 +3,30 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
-
+from django.contrib.auth.models import BaseUserManager
 # -----------------------
 # User Model
 # -----------------------
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
 class User(AbstractUser):
     ROLE_CHOICES = [
         ('guest', 'Guest'),
@@ -26,6 +46,8 @@ class User(AbstractUser):
     username = None
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
 
     def __str__(self):
         return f"{self.email} ({self.role})"
--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -7,12 +7,3 @@ urlpatterns = [
     path('api/', include('chats.urls')),            # chats API endpoints
     path('api-auth/', include('rest_framework.urls')),  # ✅ DRF browsable API login/logout
 ]
-# messaging_app/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('chats.urls')),            # chats API endpoints
-    path('api-auth/', include('rest_framework.urls')),  # ✅ DRF browsable API login/logout
-]
