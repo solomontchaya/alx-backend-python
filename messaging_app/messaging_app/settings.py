98d13a652ed62cc8a8cc42d625551290fab5472e
Happiness Obochi <obochihappiness180@gmail.com>
2025-09-27 10:16:55 +0100

    pagination issue fixed

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -65,8 +65,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'chats.pagination.MessagePagination',  # ✅ custom paginator
-    'PAGE_SIZE': 20,  
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 
 }
