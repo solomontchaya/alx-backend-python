6e7b6869defed94657560106529ac58710786a7d
Happiness Obochi <obochihappiness180@gmail.com>
2025-09-27 10:13:09 +0100

    Filters issue fixed

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -67,6 +67,8 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'chats.pagination.MessagePagination',  # ✅ custom paginator
     'PAGE_SIZE': 20,  
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+
 }
 
 
