c6f2e4ae8a3b002a0ff10f0499a6d4085e0fe03d
Solomon Tchaya <tchayasolomon2000@gmail.com>
2025-09-28 21:06:57 +0200

    Week 5

--- django-middleware-0x03/messaging_app/settings.py
+++ django-middleware-0x03/messaging_app/settings.py
@@ -51,6 +51,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # Custom Middleware
+    'chats.middleware.RequestLoggingMiddleware',
 ]
 
 ROOT_URLCONF = 'messaging_app.urls'
